539b176b6272e0f99b430def95b049acb27db556
Sammy <s.challinor3@gmail.com>
2021-05-20 23:40:19 +0100

    Saving the script

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello world!")
+/* template:
+MY _____ IS LIKE A ______
+LOOKING FOR _______
+*/ 
+
+let Array1=["heart", "life", "soul"]
+let Array2=["butterfly","bird","tortoise"]
+let Array3=["love", "romance", "truth"]
+
+const GenerateMessage = () => {
+    let Rand1=Math.floor(Math.random()*3)
+    let Rand2=Math.floor(Math.random()*3)
+    let Rand3=Math.floor(Math.random()*3)
+    
+    let Messsage = `My ${Array1[Rand1]} is like a ${Array2[Rand2]} looking for ${Array3[Rand3]}.`
+    return Message
+}
+
+console.log(GenerateMessage())
