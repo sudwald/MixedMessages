4025c2e6e9b89849672b539c230ced764124e753
Sammy <s.challinor3@gmail.com>
2021-05-20 23:49:11 +0100

    Final project

--- script.js
+++ script.js
@@ -11,9 +11,8 @@ const GenerateMessage = () => {
     let Rand1=Math.floor(Math.random()*3)
     let Rand2=Math.floor(Math.random()*3)
     let Rand3=Math.floor(Math.random()*3)
-    
-    let Messsage = `My ${Array1[Rand1]} is like a ${Array2[Rand2]} looking for ${Array3[Rand3]}.`
-    return Message
+    let GeneratedMessage = `My ${Array1[Rand1]} is like a ${Array2[Rand2]} looking for ${Array3[Rand3]}.`
+    return GeneratedMessage
 }
 
 console.log(GenerateMessage())
